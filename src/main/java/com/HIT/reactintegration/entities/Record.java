2098bb59592cd6b0732bf21515a068c5a9b5b9cd
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-08 02:31:16 -0300

    Add record content

--- src/main/java/com/HIT/reactintegration/entities/Record.java
+++ src/main/java/com/HIT/reactintegration/entities/Record.java
@@ -1,20 +1,27 @@
 package com.HIT.reactintegration.entities;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import java.util.UUID;
 
-@Entity
+@Entity(name = "RECORD")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class Record {
 
     @Id
+    @Column(name = "id")
     private UUID id = UUID.randomUUID();
 
+    @Column(name = "record_content", nullable = false, unique = true)
+    private String recordContent;
+
+    public Record(String recordContent) {
+        this.recordContent = recordContent;
+    }
 }
