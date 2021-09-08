64980fd7ce27bcf741750660b63b74bed60bfe2d
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-08 13:11:46 -0300

    Add General API information

--- src/main/java/com/HIT/reactintegration/ReactintegrationApplication.java
+++ src/main/java/com/HIT/reactintegration/ReactintegrationApplication.java
@@ -1,9 +1,21 @@
 package com.HIT.reactintegration;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(info =
+@Info(
+		title = "Records API",
+		description = "This is an API for chore list recording.",
+		version = "0.1",
+		contact = @Contact(name = "Me", email = "mimail@email.com", url = "www.misitio.com")),
+		security = @SecurityRequirement(name = "None", scopes = "All")
+)
 public class ReactintegrationApplication {
 
 	public static void main(String[] args) {
