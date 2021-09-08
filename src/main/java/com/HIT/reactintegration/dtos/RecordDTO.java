456c301d5f66d53701bb73ee38977b2cb9e7f520
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-08 12:32:36 -0300

    Add aux record DTO

---
+++ src/main/java/com/HIT/reactintegration/dtos/RecordDTO.java
@@ -0,0 +1,14 @@
+package com.HIT.reactintegration.dtos;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data @Builder
+@NoArgsConstructor @AllArgsConstructor
+public class RecordDTO {
+
+    private String recordContent;
+
+}
