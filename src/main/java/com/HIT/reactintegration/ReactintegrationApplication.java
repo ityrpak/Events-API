0a69eb8db2043eed965422feb6df2288efab2c5a
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-08 00:09:29 -0300

    Initial Commit

---
+++ src/main/java/com/HIT/reactintegration/ReactintegrationApplication.java
@@ -0,0 +1,13 @@
+package com.HIT.reactintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactintegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactintegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/HIT/reactintegration/ReactintegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.HIT.reactintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReactintegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
