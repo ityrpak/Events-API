516b845d50c290dd17f55f47d3701159e484c633
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-08 12:32:19 -0300

    Fix date format

---
+++ src/main/java/com/HIT/reactintegration/dtos/RecordWDateDTO.java
@@ -0,0 +1,21 @@
+package com.HIT.reactintegration.dtos;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.time.LocalDateTime;
+
+@Data @Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class RecordWDateDTO {
+
+    private String recordContent;
+    @JsonFormat(pattern = "dd/MM/yyyy'T'HH:MM:SS")
+    private LocalDateTime createdAt;
+
+}
