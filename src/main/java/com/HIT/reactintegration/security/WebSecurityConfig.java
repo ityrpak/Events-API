4464fd9e97b243c76ce9d23a9ffc1f0077c8214b
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-16 18:28:36 -0300

    Add Web Security Config

---
+++ src/main/java/com/HIT/reactintegration/security/WebSecurityConfig.java
@@ -0,0 +1,23 @@
+package com.HIT.reactintegration.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+
+@EnableWebSecurity
+@Configuration
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests().antMatchers("/").permitAll();
+    }
+}
