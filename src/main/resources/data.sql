69c7072598e85d688d885d9d4b6c71947f4ae7c4
Héctor Iván Tyrpak <ivan.tyrpak@gmail.com>
2021-09-18 00:32:22 -0300

    Add bcrypt format to seeder

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -6,13 +6,18 @@ VALUES
 ('ROLE_ADMIN');
 
 INSERT INTO users
-("nickname", "password", "email", "first_name", "last_name", "role_id")
+("nickname", "password", "email", "first_name", "last_name", "role_id", "enabled")
 VALUES
-('usuario1', 'usuario1', 'usuario1@gmail.com', 'Ivan', 'Tyrpak', (SELECT roles.id FROM roles WHERE role_name='ROLE_ADMIN')),
-('usuario2', 'usuario2', 'usuario2@gmail.com', 'Juan', 'Perez', (SELECT roles.id FROM roles WHERE role_name='ROLE_MODERATOR')),
-('usuario3', 'usuario3', 'usuario3@gmail.com', 'Andres', 'Fuegos', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER')),
-('usuario4', 'usuario4', 'usuario4@gmail.com', 'Leandro', 'Gutierrez', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER')),
-('usuario5', 'usuario5', 'usuario5@gmail.com', 'Pedro', 'Gomez', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER'));
+('usuario1', '{bcrypt}$2a$05$/Q0aIC8sgwU0n.NLhdgBle.LAY7qw0N34KGEvwJ6cmLgOrF0oozRO',
+ 'usuario1@gmail.com', 'Ivan', 'Tyrpak', (SELECT roles.id FROM roles WHERE role_name='ROLE_ADMIN'), 1),
+('usuario2', '{bcrypt}$2a$05$KINlpu/2HSCxMbHNLIsI7uHSLekU5NcM4G9/mW.hIQx4vzTQ8vvm6',
+ 'usuario2@gmail.com', 'Juan', 'Perez', (SELECT roles.id FROM roles WHERE role_name='ROLE_MODERATOR'), 1),
+('usuario3', '{bcrypt}$2a$05$xtL9Nu81SlcMIYgJnTEZAOP/sTqID5XXoG2omSKe/NjfINzOJAbfG',
+ 'usuario3@gmail.com', 'Andres', 'Fuegos', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER'), 1),
+('usuario4', '{bcrypt}$2a$05$ca4mglDOpuRvzwKIUUO9JO70SEASbPQEtKkIhQWiNDg4JN6.GovBe',
+ 'usuario4@gmail.com', 'Leandro', 'Gutierrez', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER'), 1),
+('usuario5', '{bcrypt}$2a$05$DMfjuHeeSy1e/EQGs03UWuQ18a09qaS6vzZXrlX2B.4twZ5beivwu',
+ 'usuario5@gmail.com', 'Pedro', 'Gomez', (SELECT roles.id FROM roles WHERE role_name='ROLE_USER'), 1);
 
 INSERT INTO events
 ("title", "description", "author_id", "created_at","last_updated")
